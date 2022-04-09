9d96fe030d037f603206922cfd1a3ee3f644ded8
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-04-09 20:59:41 +0100

    Added tests with cases for GetByFIlter

--- book/service/book_test.go
+++ book/service/book_test.go
@@ -75,3 +75,48 @@ func TestGet(t *testing.T) {
 		bookRepo.AssertExpectations(t)
 	})
 }
+
+func TestGetByFilter(t *testing.T) {
+	as := assert.New(t)
+	bookRepo := &repository.BookRepositoryMock{}
+	filter, filterValue := "title", "test"
+	t.Run("happy path: Successfully fetches book by filter", func(t *testing.T) {
+		bookRepo.On("GetByFilter", context.Background(), filter, filterValue).Return([]domain.Book{
+			{
+				ID:          1,
+				ISBN:        "978160309028",
+				Title:       "About test",
+				Description: "How to write unit test",
+			},
+			{
+				ID:          2,
+				ISBN:        "978160309029",
+				Title:       "Testing in golang",
+				Description: "Creating a go file with the _test in filename",
+			},
+		}, nil).Once()
+		service := NewBookService(bookRepo)
+		book, err := service.GetByFilter(context.Background(), filter, filterValue)
+		as.NoError(err)
+		as.Equal(len(book), 2)
+		bookRepo.AssertExpectations(t)
+	})
+
+	t.Run("input error: Book matches not found", func(t *testing.T) {
+		bookRepo.On("GetByFilter", context.Background(), filter, filterValue).Return([]domain.Book{}, domain.ErrBookNotFound).Once()
+		service := NewBookService(bookRepo)
+		book, err := service.GetByFilter(context.Background(), filter, filterValue)
+		as.Error(err)
+		as.Equal(len(book), 0)
+		bookRepo.AssertExpectations(t)
+	})
+
+	t.Run("system error: Database failed", func(t *testing.T) {
+		bookRepo.On("GetByFilter", context.Background(), filter, filterValue).Return([]domain.Book{}, errors.New("Something failed")).Once()
+		service := NewBookService(bookRepo)
+		book, err := service.GetByFilter(context.Background(), filter, filterValue)
+		as.Error(err)
+		as.Equal(len(book), 0)
+		bookRepo.AssertExpectations(t)
+	})
+}
